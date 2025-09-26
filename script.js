b3d2673020aab92c48a6a3e584242becaaf8581f
mingming0324 <alsehfdl14@gmail.com>
2025-09-26 18:11:41 +0900

    디자인 제작완

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// main slide
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  direction: getDirection(),
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
