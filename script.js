f547a7f432c98dbb6bc48a5d0643bbc919966f36
mingming0324 <alsehfdl14@gmail.com>
2025-09-30 12:11:56 +0900

    sec-1,3 tap

--- script.js
+++ script.js
@@ -2,22 +2,40 @@
 // main slide
 var swiper = new Swiper('.swiper', {
   slidesPerView: 1,
-  direction: getDirection(),
+  spaceBetween: 4,
+  observer: true,
   loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  on: {
-    resize: function () {
-      swiper.changeDirection(getDirection());
-    },
-  },
 });
 
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+//tap-menu 
+$(".best-menu li").click(function() {
+  $(".best-menu li span").removeClass("active"); // 전체 초기화
+  $(this).find("span").toggleClass("active");    // 클릭한 것만 토글
 
-  return direction;
-}
+   // 모든 영역 숨기기
+    $(".sec-1_img-wrap").removeClass("active-2");
+
+   // 클릭한 메뉴의 data-target 값 가져오기
+    let target = $(this).find("span").data("target");
+
+   // 해당 클래스만 보여주기
+    $("." + target).addClass("active-2");
+});
+
+$(".new-menu li").click(function() {
+  $(".new-menu li span").removeClass("active"); // 전체 초기화
+  $(this).find("span").toggleClass("active");    // 클릭한 것만 토글
+
+   // 모든 영역 숨기기
+    $(".sec-3_img-wrap").removeClass("active-4");
+
+   // 클릭한 메뉴의 data-target 값 가져오기
+    let target = $(this).find("span").data("target");
+
+   // 해당 클래스만 보여주기
+    $("." + target).addClass("active-4");
+});
